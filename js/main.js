d45d2b15e9c9db6460463755d84dce80c1f2957b
jolmateu <jolmateu@byui.edu>
2021-10-09 00:02:00 -0500

    Week 04
    Week 04

--- js/main.js
+++ js/main.js
@@ -1,17 +1,21 @@
 const links = [
-    {
-      label: "Week1 notes",
-      url: "week1/index.html"
-    },
-    {
-        label: "Week2 notes",
-        url: "week2/index.html"
-    },
-    {
-      label: "Week3 notes",
-      url: "week3/index.html"
+  {
+    label: "Week1 notes",
+    url: "week1/index.html"
+  },
+  {
+    label: "Week2 notes",
+    url: "week2/index.html"
+  },
+  {
+    label: "Week3 notes",
+    url: "week3/index.html"
+  },
+  {
+    label: "Week4 notes",
+    url: "week4/index.html"
   }
-  ]
+]
 
 for (let link in links){
     var a = document.createElement("a");
