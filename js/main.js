998f778abfe336d84d8283c9be606b71b891574f
jolmateu <jolmateu@byui.edu>
2021-11-08 20:41:13 -0500

    Week 08 U
    Week 08 U

--- js/main.js
+++ js/main.js
@@ -44,3 +44,56 @@ for (let link in links){
     olist.appendChild(newItem);
 }
 
+var canvas = document.getElementById("myCanvas");
+var context = canvas.getContext("2d");
+context.strokeStyle = "red";
+context.fillStyle = "rgba(0, 0, 255, 0.5)";
+context.fillRect(10, 10, 100, 100);   
+context.strokeRect(10, 10, 100, 100);
+
+function drawPattern() {
+    var canvas = document.getElementById("demo2");
+    var context = canvas.getContext("2d");
+    context.strokeStyle = "red";
+
+    var img = new Image(); 
+    img.src = "../images/bike_sprite.png";
+    img.onload = function() { 
+        var pattern = context.createPattern(img, "repeat"); 
+        context.fillStyle = pattern;                        
+        context.fillRect(10, 10, 100, 100);                  
+        context.strokeRect(10, 10, 100, 100);
+    }; 
+}
+
+function drawGradient() {
+    var canvas = document.getElementById("demo3");
+    var context = canvas.getContext("2d");
+    context.strokeStyle = "red";
+    var gradient = context.createLinearGradient(0, 0, 0, 200);
+    gradient.addColorStop(0, "blue"); 
+    gradient.addColorStop(1, "white"); 
+    context.fillStyle = gradient; 
+    context.fillRect(10, 10, 100, 100); 
+    context.strokeRect(10, 10, 100, 100); 
+}
+
+function drawCircle(canvas) {
+    var context = canvas.getContext("2d");
+    context.beginPath();
+    context.arc(100, 100, 50, 0, Math.PI*2, true);
+    context.closePath();
+    context.strokeStyle = "red";
+    context.fillStyle = "blue";
+    context.lineWidth = 3;
+    context.fill(); 
+    context.stroke(); 
+}
+
+function saveDrawing() {
+    var canvas5 = document.getElementById("demo5");
+    window.open(canvas5.toDataURL("image/png"));
+}
+
+var button = document.getElementById("saveButton");
+button.addEventListener("click", saveDrawing, false);
